cf01e7670c2a6a4fd77f2c71ad666ecee1f6fdc6
Ben Brandau <codingbrandau@gmail.com>
2021-08-20 22:13:12 -0400

    starter

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> This should update test and i have a working Android emulator</Text>
       <StatusBar style="auto" />
     </View>
   );
